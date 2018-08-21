4e8d7f9cc2a5d7735f1b0bfa2655a857998571be
huanghai <huanghai@zhongan.com>
2018-08-21 18:41:49 +0800

    1

--- cmdb/views.py
+++ cmdb/views.py
@@ -12,11 +12,11 @@ def addUser(request):
     # return HttpResponse('Hello world!')
 
     if request.method == 'POST':
-        user = request.POST.get('username',None)
-        pswd = request.POST.get('password',None)
+        user = request.POST.get('username', None)
+        pswd = request.POST.get('password', None)
         if user not in [each.user for each in models.UserInfo.objects.all()]:
             if pswd:
-                models.UserInfo.objects.create(user=user,pswd=pswd)
+                models.UserInfo.objects.create(user=user, pswd=pswd)
                 return render(request, 'addUser.html', {'data': models.UserInfo.objects.all()})
             else:
                 return HttpResponse("密码不能为空")
@@ -24,6 +24,7 @@ def addUser(request):
     else:       # 返回页面
         return render(request, 'addUser.html', )
 
+
 def rt_json(request):       # 返回json
-    test = {'name':'晃晃','age':21}
+    test = {'name': '晃晃', 'age': 21}
     return HttpResponse(json.dumps(test), content_type="application/json")
