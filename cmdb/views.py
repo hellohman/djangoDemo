09a57cc5a877f09a9bd8652cd5c6a7ecfb31fcd6
huanghai <huanghai@zhongan.com>
2018-08-24 17:55:39 +0800

    1

--- cmdb/views.py
+++ cmdb/views.py
@@ -11,12 +11,9 @@ def addUser(request):
         user = request.POST.get('user', None)
         pswd = request.POST.get('pswd', None)
         if user not in [each.user for each in models.UserInfo.objects.all()]:
-            if pswd:
-                # models.UserInfo.objects.create(user=user, pswd=pswd)
-                data = [{'user': each.user, 'pswd': each.pswd} for each in models.UserInfo.objects.all()]
-                return render(request, 'addUser.html', {'data': data})
-            else:
-                return HttpResponse("密码不能为空")
+            # models.UserInfo.objects.create(user=user, pswd=pswd)
+            data = [{'user': each.user, 'pswd': each.pswd} for each in models.UserInfo.objects.all()]
+            return render(request, 'addUser.html', {'data': data})
         return HttpResponse("用户名已存在")
     else:       # 返回页面
         return render(request, 'addUser.html', )
@@ -27,12 +24,9 @@ def add(request):
         user = request.POST.get('user', None)
         pswd = request.POST.get('pswd', None)
         if user not in [each.user for each in models.UserInfo.objects.all()]:
-            if pswd:
-                models.UserInfo.objects.create(user=user, pswd=pswd)
-                data = [{'user': each.user, 'pswd': each.pswd} for each in models.UserInfo.objects.all()]
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            else:
-                return HttpResponse("密码不能为空")
+            models.UserInfo.objects.create(user=user, pswd=pswd)
+            data = [{'user': each.user, 'pswd': each.pswd} for each in models.UserInfo.objects.all()]
+            return HttpResponse(json.dumps(data), content_type="application/json")
         return HttpResponse("用户名已存在")
     else:       # 返回页面
         return render(request, 'addUser.html', )
