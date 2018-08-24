4777ad8461d87e10b32cf32e278f9cd091638d9b
huanghai <huanghai@zhongan.com>
2018-08-24 11:17:00 +0800

    1

--- static/addUser.js
+++ static/addUser.js
@@ -1,59 +1,20 @@
+// 提交
 function submitForm(){
     $('#ff').form('submit', {
         url: '/add/',
         success: function (result) {
-            // $('#dg').datagrid({
-            //     data: data,
-            // });
-            var xxx =
-            $('#dg').datagrid('load',{'user':1,'pswd':2});
-
-            $('#dg').datagrid({
-                columns:[[
-                    // {field:'user',title:'用户名',width:350},
-                    {field:'pswd',title:'密码',width:350}
-                ]],
-                method: 'get',
-                pagination: true,
-                rownumbers: true,
-                singleSelect: true,
-                fitColumns: true,
-                selectOnCheck: true
-            });
-            var pager = $('#dg').datagrid('getPager');
-            pager.pagination({
-                showPageList:true,
-                pageSize: 10,
-                pageList: [10,20,50,100],
-                buttons:[{
-                    iconCls:'icon-search',
-                    handler:function(){
-                        alert('search');
-                    }
-                },{
-                    iconCls:'icon-add',
-                    handler:function(){
-                        alert('add');
-                    }
-                },{
-                    iconCls:'icon-edit',
-                    handler:function(){
-                        alert('edit');
-                    }
-                }],
-                onBeforeRefresh:function(){
-                    alert('before refresh');
-                    return true;
-                }
-            });
+            var result = eval('(' + result + ')');  // 转str
+            $('#dg').datagrid('loadData',{'total':2,'rows':result});
         }
     })
 }
 
+// 清空
 function clearForm(){
     $('#ff').form('clear');
 }
 
+// 数据网格
 $(function(){
     $('#dg').datagrid({
         columns:[[
