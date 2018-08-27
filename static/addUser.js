f58ec3715f1215b292b8e7dfe4859add8b9732d2
huanghai <huanghai@zhongan.com>
2018-08-27 16:52:06 +0800

    1

--- static/addUser.js
+++ static/addUser.js
@@ -15,16 +15,30 @@ function submitForm(){
             var username = tempArray[0].value;
             var password = tempArray[1].value;
             if (password.length < 8) {
-                // alert(password);
                 return true;
             }
             return false;
         },
         success: function (result) {
-            var result = eval('(' + result + ')');  // 转str
-            $('#dg').datagrid({
-                data: result
-            });
+            if (result == '用户名已存在') {
+                clearForm();
+                alert('用户名已存在,请重新输入!');
+            } else {
+                $("#div-dg").addClass('visible');       // 隐藏
+                var result = eval('(' + result + ')');   // 转json
+                $('#dg').datagrid({
+                    data: result
+                });
+                var pager = $('#dg').datagrid('getPager');
+                pager.pagination({
+                    showPageList:true,
+                    pageSize: 5,
+                    pageList: [5,10,20,50,100],
+                    beforePageText: '第',
+                    afterPageText:'页.共 {pages} 页',
+                    displayMsg:'显示 {from}-{to} 条. 共 {total} 条'
+                })
+            }
         }
     })
 }
@@ -34,14 +48,41 @@ function clearForm(){
     $('#ff').form('clear');
 }
 
+// 添加用户
+function addCustomer() {
+    // alert("xxx");
+    $.post('/add/',{'user':'xxxxx','pswd':'xxx'},function (result) {
+        alert(result);
+    })
+}
+
 // 数据网格
 $(function(){
     $('#dg').datagrid({
         columns:[[
-            {field:'ck',checkbox:true},     // 复选框
+            {field:'ck',title:'选择',checkbox:true},     // 复选框
             {field:'user',title:'用户名',width:350,align:'center',halign:'center',resizable:true},
             {field:'pswd',title:'密码',width:350,align:'center',halign:'center',resizable:true}
         ]],
+        toolbar: [{
+            text:'添加用户',
+            iconCls: 'icon-add',
+            handler: function(){
+                addCustomer();
+            }
+        },'-',{
+            text:'修改用户',
+            iconCls: 'icon-edit',
+            handler: function(){
+                updateCustomer();
+            }
+        },'-',{
+            text:'删除用户',
+            iconCls: 'icon-remove',
+            handler: function(){
+                delCustomer();
+            }
+        }],
         method: 'get',
         pagination: true,
         rownumbers: true,
@@ -50,30 +91,4 @@ $(function(){
         fitColumns: true,
         selectOnCheck: true
     });
-    var pager = $('#dg').datagrid('getPager');
-    pager.pagination({
-        showPageList:true,
-        pageSize: 5,
-        pageList: [5,10,20,50,100],
-        buttons:[{
-            iconCls:'icon-search',
-            handler:function(){
-                alert('search');
-            }
-        },{
-            iconCls:'icon-add',
-            handler:function(){
-                alert('add');
-            }
-        },{
-            iconCls:'icon-edit',
-            handler:function(){
-                alert('edit');
-            }
-        }],
-        onBeforeRefresh:function(){
-            alert('before refresh');
-            return true;
-        }
-    })
 });
