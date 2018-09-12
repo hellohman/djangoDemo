f03ba915ca0419ace878b34d02b35bc5b3acb719
huanghai <huanghai@zhongan.com>
2018-09-12 18:22:58 +0800

    1

--- cmdb/HhHelper/paramsHelper.py
+++ cmdb/HhHelper/paramsHelper.py
@@ -4,10 +4,10 @@ import datetime
 def dl_beginAndStopDate(beginDate=None, stopDate=None):
     beginArr, stopArr = (1753, 1, 1), (9999, 12, 31)
     if beginDate and stopDate:
-        beginArr = (int(aa) if aa[0] != '0' else int(aa[1]) for aa in beginDate.split("-"))
-        stopArr = (int(aa) if aa[0] != '0' else int(aa[1]) for aa in stopDate.split("-"))
+        beginArr = (int(aa) for aa in beginDate.split("-"))
+        stopArr = (int(aa) for aa in stopDate.split("-"))
     elif beginDate:
-        beginArr = (int(aa) if aa[0] != '0' else int(aa[1]) for aa in beginDate.split("-"))
+        beginArr = (int(aa) for aa in beginDate.split("-"))
     elif stopDate:
-        stopArr = (int(aa) if aa[0] != '0' else int(aa[1]) for aa in stopDate.split("-"))
+        stopArr = (int(aa) for aa in stopDate.split("-"))
     return datetime.date(*beginArr), datetime.date(*stopArr)
